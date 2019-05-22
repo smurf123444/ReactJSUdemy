41424de269fb6021bdcd6a780e4540c9cf55beca
Chris <chris@Chriss-iMac.fios-router.home>
2019-05-22 11:13:45 -0400

    state manipulation with JS

--- Assignment1Udemy/src/App.js
+++ Assignment1Udemy/src/App.js
@@ -1,87 +1,82 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './components/Person'
+
 import UserInput from './components/UserInput'
 import UserOutput from './components/UserOutput'
 
 class App extends Component {
   state = {
       userinput0: [
-        { string: '' },
-      ],
-      userinput1: [
-        { string: ''},
-      ],
-      userinput2: [
-        { string: '' },
+        { id: 'test', string: '' },
+        { id: 'test1', string: '' },
+        { id: 'test2', string: '' },
       ],
       otherState: 'some test state',
   }
   
-switchNameHandler = () => {
+switchNameHandler = (event) => {
     // DONT DO THIS this.state.persons[0].name = 'Test'
     this.setState({
       userinput0: [
-        { string: '' },
-      ],
-      userinput1: [
-        { string: ''},
-      ],
-      userinput2: [
-        { string: '' },
-      ],
-    otherState: 'some test state',
+        { id: 'test', string: '' },
+        { id: 'test1', string: '' },
+        { id: 'test2', string: '' },
+    ],
 })
   }
 
-userChangeHandler0 = (event) => {
+userChangeHandler0 = (event, id) => {
     // DONT DO THIS this.state.persons[0].name = 'Test'
+
+    const personIndex = this.state.userinput0.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.userinput0[personIndex]
+    };
+
+    person.string = event.target.value;
+
+    const persons = [...this.state.userinput0];
+    persons[personIndex] = person;
+
     this.setState({ 
-    userinput0: [
-      { string: event.target.value },
-    ],
-    otherState: 'some test state',
-})}
+    userinput0: persons
+  } )
+}
 
-userChangeHandler1 = (event) => {
-  // DONT DO THIS this.state.persons[0].name = 'Test'
-  this.setState({ 
-  userinput1: [
-    { string: event.target.value },
-  ],
-  otherState: 'some test state',
-})}
-
-userChangeHandler2 = (event) => {
-  // DONT DO THIS this.state.persons[0].name = 'Test'
-  this.setState({ 
-  userinput2: [
-    { string: event.target.value },
-  ],
-  otherState: 'some test state',
-})}
-
+togglePersonsHandler = () => {
+  this.setState({
+    showPersons: true
+  })
+}
 
   render() {
+    let persons = null;
+    if (this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.userinput0.map((person) => {
+            return <UserInput 
+              value={person.string} 
+              changed={(event) => this.userChangeHandler0(event, person.id)}/>
+          })}
+
+          {this.state.userinput0.map(person => {
+            return <UserOutput
+              value={person.string} 
+              changed={(event) => this.userChangeHandler0(event, person.id)}/>
+          })}
+        </div>
+      )
+    }
       return (
         <div className='App'> 
-
+        <button onClick={this.togglePersonsHandler}>Toggle Person</button>
         <button onClick={this.switchNameHandler}>Reset</button>
         
-        <UserInput value={this.state.userinput0[0].string} 
-                   changed={this.userChangeHandler0}/>
-        <UserInput value={this.state.userinput1[0].string} 
-                   changed={this.userChangeHandler1}/>  
-        <UserInput value={this.state.userinput2[0].string} 
-                   changed={this.userChangeHandler2}/>        
-    
-        <UserOutput value={this.state.userinput0[0].string}
-                    changed={this.userChangeHandler0} />
-        <UserOutput value={this.state.userinput1[0].string}
-                    changed={this.userChangeHandler1} />
-        <UserOutput value={this.state.userinput2[0].string}
-                    changed={this.userChangeHandler2} />
-
+       {persons}
         </div>
       );
     }
